85a74bae21fdd0045bea3f3fd6b99d2e5856897e
Ash Kyd <kyd.ashley@abc.net.au>
2025-04-30 08:52:00 +1000

    bug: launch timeout

--- src/lib/browser.server.js
+++ src/lib/browser.server.js
@@ -3,22 +3,29 @@ import chromium from '@sparticuz/chromium-min';
 import puppeteer from 'puppeteer-core';
 
 async function getBrowser() {
+	const isLocal = env.IS_LOCAL === '1';
 
-  const isLocal = env.IS_LOCAL === '1';
-
-  const args = [...(isLocal? puppeteer.defaultArgs(): chromium.args), '--hide-scrollbars', '--disable-web-security'];
-  const defaultViewport = chromium.defaultViewport;
-  const executablePath = isLocal ? env.BROWSER_PATH : await chromium.executablePath(
-      `https://github.com/Sparticuz/chromium/releases/download/v130.0.0/chromium-v130.0.0-pack.tar`
-    )
-    console.log('executablePath :>> ', executablePath);
-  return puppeteer.launch({
-    args,
-    defaultViewport,
-    executablePath,
-    headless: isLocal ? false : chromium.headless,
-    // ignoreHTTPSErrors: true,
-  });
+	const args = [
+		...(isLocal ? puppeteer.defaultArgs() : chromium.args),
+		'--hide-scrollbars',
+		'--disable-web-security'
+	];
+	const defaultViewport = chromium.defaultViewport;
+	const executablePath = isLocal
+		? env.BROWSER_PATH
+		: await chromium.executablePath(
+				`https://github.com/Sparticuz/chromium/releases/download/v130.0.0/chromium-v130.0.0-pack.tar`
+			);
+	console.log('executablePath :>> ', executablePath);
+	return puppeteer.launch({
+		args,
+		defaultViewport,
+		executablePath,
+		headless: isLocal ? false : chromium.headless,
+		// ignoreHTTPSErrors: true,
+		protocolTimeout: 1000 * 60,
+		timeout: 1000 * 60
+	});
 }
 
 /**
@@ -28,14 +35,13 @@ async function getBrowser() {
  * @param {number} [width] Set the viewport width before taking screenshot (default: `810`)
  * @param {number} [height] Set the viewport height before taking screenshot (default: `1080`)
  */
-export async function getScreenshot(url, selector = 'body', width = 810, height = 1080 ) {
-  const viewport = { width, height, deviceScaleFactor: 2 };
-  const browser = await getBrowser();
-  const page = await browser.newPage();
-  await page.setViewport(viewport);
-  await page.goto(url, { waitUntil: "networkidle0" });
-  const el = await page.waitForSelector(selector);
-  const file = await el?.screenshot();
-  return file;
+export async function getScreenshot(url, selector = 'body', width = 810, height = 1080) {
+	const viewport = { width, height, deviceScaleFactor: 2 };
+	const browser = await getBrowser();
+	const page = await browser.newPage();
+	await page.setViewport(viewport);
+	await page.goto(url, { waitUntil: 'networkidle0' });
+	const el = await page.waitForSelector(selector);
+	const file = await el?.screenshot();
+	return file;
 }
-
